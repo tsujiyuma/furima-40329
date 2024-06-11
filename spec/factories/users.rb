ac0fa29c71225ea772c491e9f14fec363ea5c44e
tsujiyuma <ktw.831.y@gmail.com>
2024-06-11 22:28:13 +0900

    テストコードの記述

---
+++ spec/factories/users.rb
@@ -0,0 +1,14 @@
+FactoryBot.define do
+  factory :user do
+    nickname              {'test'}
+    email                 {'test@example'}
+    password              {'000000'}
+    password_confirmation {password}
+    family_name_kanji     {'山田'}
+    first_name_kanji      {'陸太郎'}
+    family_name_kana      {'ヤマダ'}
+    first_name_kana       {'リクタロウ'}
+    birthday              {'2020-01-01'}
+
+  end
+end
