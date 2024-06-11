6c70a21fbcde8531fd8b5c4bebfe1502744bdcbd
tsujiyuma <ktw.831.y@gmail.com>
2024-06-11 20:09:32 +0900

    テーブル(カラム名修正)

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_06_09_073926) do
+ActiveRecord::Schema[7.0].define(version: 2024_06_11_110737) do
   create_table "users", charset: "utf8", force: :cascade do |t|
     t.string "nickname", default: "", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "family_name_kanji", default: "", null: false
-    t.string "frist_name_kanji", default: "", null: false
+    t.string "first_name_kanji", default: "", null: false
     t.string "family_name_kana", default: "", null: false
     t.string "first_name_kana", default: "", null: false
     t.date "birthday", null: false
