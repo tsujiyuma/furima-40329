282fc1850b5d018db57e34fb131accf361eca9e5
tsujiyuma <ktw.831.y@gmail.com>
2024-06-13 19:28:59 +0900

    ビューの遷移とリスト表示の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,24 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name,:description,:category_id,:status_id,:shipping_fee_id,:prefecture_id,:shipping_day_id,:price)
+  end
+
 end
