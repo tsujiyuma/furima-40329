e10542f58db9c0f6459212ea36c5bf7da3f00f54
tsujiyuma <ktw.831.y@gmail.com>
2024-06-16 16:51:52 +0900

    コントローラー、モデル(ログイン有無、integerバリ)

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,8 @@
 class ItemsController < ApplicationController
+
+  before_action :authenticate_user!, only: [:new, :create]
+
+
   def index
   end
 
--- app/models/item.rb
+++ app/models/item.rb
@@ -14,6 +14,6 @@ class Item < ApplicationRecord
   validates :shipping_fee_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
   validates :shipping_day_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
+  validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
 
 end
