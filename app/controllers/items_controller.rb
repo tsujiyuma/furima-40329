501deca906a4d9649b0e33fe1da11cfc1e1d7c0b
tsujiyuma <ktw.831.y@gmail.com>
2024-06-21 19:58:52 +0900

    コメント修正1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :edit]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :destroy]
   before_action :item_find, only: [:show, :edit, :update, :destroy]
 
   def index
