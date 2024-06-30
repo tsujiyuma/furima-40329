8d1d11ad4e880ba43e1d94cdc9ea77e08803f0db
tsujiyuma <ktw.831.y@gmail.com>
2024-06-30 16:38:20 +0900

    クレジット決済の実装

--- app/models/order_purchase.rb
+++ app/models/order_purchase.rb
@@ -1,12 +1,13 @@
 class OrderPurchase
   include ActiveModel::Model
-  attr_accessor :post_code, :prefecture_id, :municipality, :street_addres, :building_name, :photo_num, :item_id, :user_id
+  attr_accessor :post_code, :prefecture_id, :municipality, :street_addres, :building_name, :photo_num, :item_id, :user_id, :token
 
   with_options presence: true do
     validates :post_code, format: { with: /\A\d{3}-\d{4}\z/, message: "はハイフンを含む7桁の数字で入力してください" }
     validates :municipality
     validates :street_addres
-    validates :photo_num,length: { minimum: 10, maximum: 11 }, numericality: { only_integer: true }, format: { with: /\A[0-9]+\z/, message:"は半角数字で入力してください" }
+    validates :photo_num, length: { minimum: 10, maximum: 11 }, numericality: { only_integer: true }, format: { with: /\A[0-9]+\z/, message:"は半角数字で入力してください" }
+    validates :token
   end
 
   validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
